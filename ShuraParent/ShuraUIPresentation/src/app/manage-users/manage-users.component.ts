410280ecf0ec7a536cb1ff42adf98af92de789bd
Ahmed-sa3d <38682139+Ahmed-sa3d@users.noreply.github.com>
2018-07-26 13:08:53 +0200

    implement user data table in FE

--- ShuraParent/ShuraUIPresentation/src/app/manage-users/manage-users.component.ts
+++ ShuraParent/ShuraUIPresentation/src/app/manage-users/manage-users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import { AddEditUsersComponent } from './add-edit-users/add-edit-users.component';
+import { User } from '../models/user.model';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-manage-users',
@@ -9,19 +11,20 @@ import { AddEditUsersComponent } from './add-edit-users/add-edit-users.component
 })
 export class ManageUsersComponent implements OnInit {
 
-  displayedColumns = ['id', 'name', 'progress', 'color', 'edit'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['username', 'email', 'password', 'edit'];
+  dataSource: MatTableDataSource<User>;
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor(private addEditUsersComponent: AddEditUsersComponent) { 
-    // Create 100 users
-    const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
+  users: User[];
+  errorMessage: string;
 
+  constructor(private addEditUsersComponent: AddEditUsersComponent,
+  private userService: UserService) { 
+    this.getUsers();
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.dataSource = new MatTableDataSource(this.users);
   }
 
   ngOnInit() {
@@ -35,32 +38,16 @@ export class ManageUsersComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
+  getUsers(): void {
+    this.userService.getUsers()
+        .subscribe(user => {
+            this.users = user;
+            this.dataSource.data = this.users;
+            console.log(JSON.stringify(this.users));
+        },
+        error => this.errorMessage = <any>error);
 }
 
-/** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
-
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
 }
 
-/** Constants used to fill up our data base. */
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
