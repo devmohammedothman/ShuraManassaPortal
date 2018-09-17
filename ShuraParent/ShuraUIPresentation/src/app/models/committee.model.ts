ab5b205b9abd1f0f91b82e77425434d16bfddf9e
devmohammedothman <mothman@sbm.net.sa>
2018-09-17 13:13:37 +0200

    Fe Commit
    set design for manage committee UI

--- ShuraParent/ShuraUIPresentation/src/app/models/committee.model.ts
+++ ShuraParent/ShuraUIPresentation/src/app/models/committee.model.ts
@@ -4,21 +4,19 @@ export class Committee
 {
     
      id?: number;
-     nameAr: string;
-     nameEn: string;
+     nameAr?: string;
+     nameEn?: string;
      commManager?: User;
      comm?: Committee;
      expList?: string[];
-    constructor(committee: Committee){
-        this.comm = committee;
-    }
     
-    // constructor(id : number , nameAr : string , nameEn : string , commManager : User)
-    // {
-    //     this.id = id;
-    //     this.nameAr = nameAr;
-    //     this.nameEn = nameEn;
-    //     this.commManager = commManager;
-    // }
+    
+    constructor(id? : number , nameAr? : string , nameEn? : string , commManager? : User)
+    {
+        this.id = id;
+        this.nameAr = nameAr;
+        this.nameEn = nameEn;
+        this.commManager = commManager;
+    }
 
 }
